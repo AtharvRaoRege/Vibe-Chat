b1f1a1dd6862ebcf9f2c64520468a0095961db4f
AaviAgrawal <aaviagrawal212@gmail.com>
2024-12-05 12:55:03 +0530

    added login

--- backend/app.js
+++ backend/app.js
@@ -6,8 +6,6 @@ var logger = require('morgan');
 require('dotenv').config();
 const db = require('./config/mongoose');
 db();
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -23,8 +21,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,8 @@
 import React from 'react'
-import Register from './Components/Register'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/register" element={<Register/>} />
-        {/* Add other routes */}
-      </Routes>
-    </Router>
+    <div>App</div>
   )
 }
 
