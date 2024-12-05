0c36cb17f553d4c5da4ffe53357483e6c6e0228f
AtharvRaoRege <atharvrege722@gmail.com>
2024-12-05 13:29:36 +0530

    re-start

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,9 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginPage from './components/Login';
-import Register from './components/Register';
+import React from 'react'
 
 const App = () => {
   return (
-    // <Router>
-    //   <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-    //     <Routes>
-    //       {/* Route for Register Page */}
-    //       <Route path="/register" element={<Register />} />
-          
-    //       {/* Route for Login Page */}
-    //       <Route path="/login" element={<LoginPage />} />
-    //     </Routes>
-    //   </div>
-    // </Router>
-    <Register/>
-  );
-};
+    <div>App</div>
+  )
+}
 
-export default App;
+export default App
